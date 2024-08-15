b30f3d35723c838c5f06519a1241ade4b84d4920
DanielDevBR <dados.linux@gmail.com>
2024-08-15 12:35:27 -0300

    added qt6-base-dev

--- deploy.sh
+++ deploy.sh
@@ -29,10 +29,11 @@ sudo apt install -y aptitude wget file bzip2 build-essential ninja-build
 pkgcachedir='/tmp/.pkgdeploycache'
 mkdir -p ${pkgcachedir}
 
-sudo aptitude -y -d -o dir::cache::archives="${pkgcachedir}" install libsdl2-2.0 libsdl2-net libhidapi-hidraw0 libhidapi-libusb0
+#sudo aptitude -y -d -o dir::cache::archives="${pkgcachedir}" install libsdl2-2.0-0 libsdl2-net libhidapi-hidraw0 libhidapi-libusb0
+sudo aptitude -y -d -o dir::cache::archives="${pkgcachedir}" install libsdl2-dev libsdl2-net-dev libhidapi-dev qt6-base-dev
 #-------------------------------------------------
 
-sudo apt install -y libsdl2-dev libsdl2-net-dev libhidapi-dev
+sudo apt install -y libsdl2-dev libsdl2-net-dev libhidapi-dev qt6-base-dev
 #######-------#######-------#######-------#######-------#######-------#######-------#######-------
 
 # Get simple64 code
