0e996ebaba822c24f0866fa28fdbceef94e10f7b
Daniel Ribeiro da Silva <danielribeiro@ufg.br>
2024-08-26 14:27:16 -0300

    test new version

--- deploy.sh
+++ deploy.sh
@@ -35,15 +35,20 @@ mkdir -p ${pkgcachedir}
 
 sudo aptitude -y -d -o dir::cache::archives="${pkgcachedir}" install libpng-dev libsdl2-dev libsdl2-net-dev libhidapi-dev libvulkan-dev qt6-base-dev qt6-websockets-dev libcurl3t64-gnutls || die "* Cant install package deps!"
 
+
+# download deb files from installed packages using aptitude
+sudo aptitude -y -d -o dir::cache::archives="${pkgcachedir}" download libcurl3t64-gnutls libssh libldap2 libsasl2 libc6 libglib2.0-dev
+
 #extras: libcurl3t64-gnutls libssh libldap2 libsasl2
-wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3t64-gnutls_8.5.0-2ubuntu10.2_amd64.deb -P $pkgcachedir
-wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/libs/libssh/libssh-4_0.10.6-2build2_amd64.deb -P $pkgcachedir
-wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/o/openldap/libldap2_2.6.7+dfsg-1~exp1ubuntu8_amd64.deb -P $pkgcachedir
-wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/c/cyrus-sasl2/libsasl2-2_2.1.28+dfsg1-5ubuntu3_amd64.deb -P $pkgcachedir
+#wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3t64-gnutls_8.5.0-2ubuntu10.2_amd64.deb -P $pkgcachedir
+#wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/libs/libssh/libssh-4_0.10.6-2build2_amd64.deb -P $pkgcachedir
+#wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/o/openldap/libldap2_2.6.7+dfsg-1~exp1ubuntu8_amd64.deb -P $pkgcachedir
+#wget -nv -c http://mirrors.kernel.org/ubuntu/pool/main/c/cyrus-sasl2/libsasl2-2_2.1.28+dfsg1-5ubuntu3_amd64.deb -P $pkgcachedir
 
 #base
-wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.39-0ubuntu8.2_amd64.deb -P $pkgcachedir
-wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-dev_2.80.0-6ubuntu3.1_amd64.deb -P $pkgcachedir
+#wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.39-0ubuntu8.2_amd64.deb -P $pkgcachedir
+#wget -nv -c http://security.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-dev_2.80.0-6ubuntu3.1_amd64.deb -P $pkgcachedir
+
 
 
 #-------------------------------------------------
@@ -84,17 +89,17 @@ cd "$WORKDIR" || die "ERROR: Directory don't exist: ${WORKDIR}"
 
 sudo chmod 777 ${pkgcachedir} -R
 
-# manual copy all usr libs
-echo "copying usr dir"
-du -hs /usr
-cp -a -rv /usr ./
+# manual copy all usr libs (cancelled because of size: 27GB)
+#echo "copying usr dir"
+#du -hs /usr
+#cp -a -rv /usr ./
 
 #extras
 #wget -nv -c http://ftp.osuosl.org/pub/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-4_amd64.deb -P $pkgcachedir
 
 
-#find ${pkgcachedir} -name '*deb' ! -name 'mesa*' -exec dpkg -x {} . \;
-#echo "All files in ${pkgcachedir}: $(ls ${pkgcachedir})"
+find ${pkgcachedir} -name '*deb' ! -name 'mesa*' -exec dpkg -x {} . \;
+echo "All files in ${pkgcachedir}: $(ls ${pkgcachedir})"
 
 #-------------------------------------------------
 
